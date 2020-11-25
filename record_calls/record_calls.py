aa7704a69cfbef3be713851e455498061f00a3c5
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-11-25 10:32:04 +0100

    record_calls

--- record_calls/record_calls.py
+++ record_calls/record_calls.py
@@ -1,4 +1,17 @@
 from functools import wraps
+from dataclasses import dataclass
+from typing import Any, Optional
+
+
+NO_RETURN = object()
+
+
+@dataclass
+class Call:
+    args: tuple
+    kwargs: dict
+    return_value: Any = NO_RETURN
+    exception: Optional[BaseException] = None
 
 
 def record_calls(original_function):
@@ -6,10 +19,17 @@ def record_calls(original_function):
     @wraps(original_function)
     def wrapper_function(*args, **kwargs):
         wrapper_function.call_count += 1
-        return original_function(*args, **kwargs)
+        call = Call(args, kwargs)
+        wrapper_function.calls.append(call)
+        try:
+            call.return_value = original_function(*args, **kwargs)
+        except BaseException as e:
+            call.exception = e
+            raise
+        return call.return_value
 
     wrapper_function.call_count = 0
-
+    wrapper_function.calls = []
     return wrapper_function
 
 # class record_calls(object):
